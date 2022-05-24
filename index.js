29efc8c4661dc9daf035faa407a86e6ad86ef3b1
pippal5536 <pippal5536@gmail.com>
2022-05-25 04:26:26 +0600

    delete method done

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ async function run() {
         console.log('Mongo db is connected')
         const collectionOfTools = client.db('tools_manufacturer').collection('tools')
         const collectionOfUsers = client.db('tools_manufacturer').collection('users');
+        const collectionOfPurchasedTools = client.db('tools_manufacturer').collection('purchased-tools');
 
         // to load tools in homepage
         app.get('/tool', async (req, res) => {
@@ -45,6 +46,19 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
 
+        })
+        // to load selected tool in purchase page
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await collectionOfTools.findOne(query);
+            res.send(tool);
+        });
+        // to upload purchased tool
+        app.post('/purchase', async (req, res) => {
+            const purchasedTools = req.body;
+            const result = await collectionOfPurchasedTools.insertOne(purchasedTools);
+            return res.send(result);
         })
         // to save registered users to database
         app.put('/user/:email', async (req, res) => {
@@ -58,9 +72,29 @@ async function run() {
             };
             const result = await collectionOfUsers.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET)
-
             res.send({ result, token });
+        })
 
+        // to load orders in my orders page
+        app.get('/purchase', verifyJWT, async (req, res) => {
+            const user = req.query.user;
+            const decodedEmail = req.decoded.email;
+            if (user === decodedEmail) {
+                const query = { user: user };
+                const purchasedTools = await collectionOfPurchasedTools.find(query).toArray();
+                return res.send(purchasedTools);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+        })
+
+        // to delete orders
+        app.delete('/purchase/:id',  async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await collectionOfPurchasedTools.deleteOne(query);
+            res.send(result);
         })
     }
 
